0b1f3e71bb6cff0a2db5281bba97550a16bcda98
davidctdavid <davidjoelrb_bsc@hotmail.com>
2020-05-16 14:36:11 -0500

    asds
    asd

--- dialogs/qnamakerBaseDialog.js
+++ dialogs/qnamakerBaseDialog.js
@@ -12,7 +12,7 @@ const { QnACardBuilder } = require('../utils/qnaCardBuilder');
 // Default parameters
 const DefaultThreshold = 0.3;
 const DefaultTopN = 3;
-const DefaultNoAnswer = 'No he logrado entenderte intenta nuevamente con otras palabras.';
+const DefaultNoAnswer = 'No he logrado entenderte intenta nuevamente con otras palabras 🗣🤳.';
 
 // Card parameters
 const DefaultCardTitle = 'Did you mean:';
